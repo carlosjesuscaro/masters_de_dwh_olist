ef531eeff12500944ac13d3b2006e737c2a4d2f8
Carlos Jesus Caro <carlos.jesus.caro@gmail.com>
2025-10-20 02:46:19 -0400

    feat: Initial STA customers table addition

---
+++ SQLQueries/STA_Queries.sql
@@ -0,0 +1,12 @@
+USE OLIST_STA
+CREATE TABLE dbo.[STA_Customers] (
+    [customer_id] varchar(50),
+    [customer_unique_id] varchar(50),
+    [customer_zip_code_prefix] varchar(50),
+    [customer_city] varchar(50),
+    [customer_state] varchar(50)
+);
+
+
+SELECT *
+FROM STA_Customers;
