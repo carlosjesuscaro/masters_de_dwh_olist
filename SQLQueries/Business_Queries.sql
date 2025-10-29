2bc31cae7c0095b241f499e23594e5eda910ba76
Carlos Jesus Caro <carlos.jesus.caro@gmail.com>
2025-10-29 03:27:32 -0400

    Defining the questions to be answered in the report

--- SQLQueries/Business_Queries.sql
+++ SQLQueries/Business_Queries.sql
@@ -13,6 +13,28 @@ WHERE
 SELECT TOP 10 *
 FROM DWH_FactOrders;
 
+-- Business questions
+
+-- 1. Rank the states based on number of orders
+
+-- 2. Rank the states based on the averagew order value
+
+-- 3. Rank the states based on average order value per customer
+
+-- 4. Rank the sellers based on average and number of orders
+
+-- 5. What sellers have the shortest delivery time
+
+-- 6. Rank the sellers based on the number of late deliveries
+
+-- 7. Rank the products based on reviews
+
+-- 8. What are the most popular categories
+
+-- 9. What is the average product value based on the number of photos per product amd/pr category?
+
+-- 10. What state has the highest average of number of installments?
+
 SELECT COUNT(DISTINCT order_id)
 FROM DWH_FactOrders;
 
